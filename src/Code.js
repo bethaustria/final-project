b48e6732f0d79ebe8e1f29b8ff8a8eaaa88700ed
bethaustria <bethaustria10@gmail.com>
2020-12-29 16:26:38 -0500

    changed href on code.js

--- src/Code.js
+++ src/Code.js
@@ -4,7 +4,7 @@ export default function Code(props) {
   return (
     <div className="code">
       <footer></footer>
-      <a href="https://github.com/bethaustria/Homework-Week-4" target="_blank">GitHub</a>
+      <a href="https://github.com/bethaustria/final-project" target="_blank">GitHub</a>
       <script src="src/script.js"></script>
       <div id="root"></div>
       </footer>
