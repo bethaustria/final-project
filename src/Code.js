07992569918ecb8dc87e1a3c8a8326fe6c526db6
bethaustria <bethaustria10@gmail.com>
2020-12-29 16:27:04 -0500

    removed unecessary footer tag

--- src/Code.js
+++ src/Code.js
@@ -3,7 +3,7 @@ import React from "react";
 export default function Code(props) {
   return (
     <div className="code">
-      <footer></footer>
+      <footer>
       <a href="https://github.com/bethaustria/final-project" target="_blank">GitHub</a>
       <script src="src/script.js"></script>
       <div id="root"></div>
