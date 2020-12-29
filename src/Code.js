56d31dae5e84eb02232f580e676cc0624ccf303a
bethaustria <bethaustria10@gmail.com>
2020-12-29 16:21:53 -0500

    created code,js

---
+++ src/Code.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function Code(props) {
+  return (
+    <div className="code">
+      <footer></footer>
+      <a href="https://github.com/bethaustria/Homework-Week-4" target="_blank">GitHub</a>
+      <script src="src/script.js"></script>
+      <div id="root"></div>
+      </footer>
+    </div>
+  );
+}
