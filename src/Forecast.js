099459044870de0f156d77c4fa7736a701ce1096
bethaustria <bethaustria10@gmail.com>
2020-12-30 14:30:15 -0500

    Create Forecast.js

---
+++ src/Forecast.js
@@ -0,0 +1,11 @@
+import React from "react";
+import Five from "./Five";
+
+export default function Forecast(props) {
+  return (
+    <div className="forecast">
+      <h4>Weather Forecast</h4>
+      <Five />
+    </div>
+  );
+}
