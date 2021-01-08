30a14e4705baec376078a6f2509141f83f199597
bethaustria <bethaustria10@gmail.com>
2021-01-08 17:26:41 -0500

    fixed the convert buttons

--- src/Weather.js
+++ src/Weather.js
@@ -1,43 +1,74 @@
 import React, { useState } from "react";
-import axios from "axios";
 import Humidity from "./Humidity";
 
 export default function Weather(props) {
   let [temperature, setTemperature] = useState(null);
-
-  function displayTemp(response) {
-    setTemperature(response.data.main.temp);
+  const [convertUnit, setUnit] = useState("celsius");
+  let temp;
+  
+  function showFahrenheit(event) {
+    event.preventDefault();
+    setUnit("fahrenheit");
   }
 
-  let urlKey = "fbcbff47d196b60aa06eb680dd916472";
-  let unit = "metric";
-  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${props.city}&appid=${urlKey}&units=${unit}`;
-  axios.get(apiUrl).then(displayTemp);
+  function showCelsius(event) {
+    event.preventDefault();
+    setUnit("celsius");
+  }
 
-  return (
-    <div className="row">
-      <div className="col-2 offset-4">
-        <div className="temp">
-          <img
-            id="icon"
-            src="https://img.icons8.com/color/64/000000/sun.png"
-            alt="weather icon"
-          />
-          <p id="temperature" className="mainTemp">
-            {Math.round(temperature)}
-          </p>
-          <span className="units">
-            <a href="/#" className="active">
-              °C
-            </a>{" "}
-            |
-            <a href="/#" id="fahrenheit">
-              °F
-            </a>
-          </span>
-        </div>
-      </div>
-      <Humidity />
-    </div>
-  );
+  if (temp = document.querySelector("#temperature")) {
+    temperature = parseInt(temp.innerText);  
+  }
+
+  console.log("Converting to " + convertUnit + " with " + temperature + " into " + (Math.round(temperature * 9 / 5) + 32));
+  if (convertUnit === 'celsius') {
+    return (
+      <div className="row">
+         <div className="col-2 offset-4">
+          <div className="temp">
+            <img
+              id="icon"
+              src="https://img.icons8.com/color/64/000000/sun.png"
+              alt="weather icon"
+            />
+            <p id="temperature" className="mainTemp">
+              { Math.round((temperature - 32) * 5/9) }
+            </p>
+            <span className="units">              
+                °C
+              {" "}
+                | <a href="/#" onClick={showFahrenheit} className="active">°F</a>         
+            </span>
+          <Humidity />
+          </div>
+         </div>
+       </div>
+    );
+  } else {
+    return (
+       <div className="row">
+         <div className="col-2 offset-4">
+          <div className="temp">
+            <img
+              id="icon"
+              src="https://img.icons8.com/color/64/000000/sun.png"
+              alt="weather icon"
+            />
+            <p id="temperature" className="mainTemp">
+              { Math.round(temperature * 9/5) + 32 }
+            </p>
+            <span className="units">
+              <a href="/#" onClick={showCelsius} className="active">
+                °C
+              </a>{" "}
+                | °F            
+            </span>
+          <Humidity />
+          </div>
+         </div>
+       </div>
+    );
+
+  }
+
 }
